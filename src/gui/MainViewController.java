87c51c135200aa79ad542702fef69103d05b8c11
SouzaDiegoCl <souzadiegocl@gmail.com>
2024-07-09 18:32:58 -0300

    About View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -28,7 +37,7 @@ public class MainViewController implements Initializable{
 	}
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/about.fxml");
 	}
 	
 	
@@ -36,5 +45,26 @@ public class MainViewController implements Initializable{
 	public void initialize(URL url, ResourceBundle rb) {
 		
 	}
+	
+	//synchronized garante que essa função não vai ser interrompida durante por conta do multi thread
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVbox = loader.load();			
+			
+			Scene mainScene = Main.getMainScene();
+			//getRoot() pega o primeiro elemento da view principal
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);//Guarda menu do main 
+			mainVBox.getChildren().clear(); //Limpa tudo do main inclusive o menu, botoes, etc
+			mainVBox.getChildren().add(mainMenu); //Adiciona o menu do main novamente
+			mainVBox.getChildren().addAll(newVbox.getChildren());//Adiciona no main o conteudo(filhos) da nova janela
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
 
 }
