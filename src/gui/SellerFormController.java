ccfc94c2b8e5f8dffedbfb393eef7d14a00b75d6
SouzaDiegoCl <souzadiegocl@gmail.com>
2024-07-18 22:02:31 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -133,6 +135,25 @@ public class SellerFormController implements Initializable {
 		}
 		obj.setName(txtName.getText());
 
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseDouble(txtBaseSalary.getText()));
+
+		obj.setDepartment(comboBoxDepartment.getValue());
+		
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
@@ -179,13 +200,12 @@ public class SellerFormController implements Initializable {
 		// LocalDate.ofInstant(1°ARG: Instant, por isso transforma o entity.getBirthDate
 		// em Instant; 2°ARG: Time-zone, aqui estamos pegando o timezone do usuário
 
-		if(entity.getDepartment() == null) {
-			 comboBoxDepartment.getSelectionModel().selectFirst();
+		if (entity.getDepartment() == null) {
+			comboBoxDepartment.getSelectionModel().selectFirst();
+		} else {
+			comboBoxDepartment.setValue(entity.getDepartment());
 		}
-		else {
-			comboBoxDepartment.setValue(entity.getDepartment());			
-		}
-		
+
 	}
 
 	public void loadAssociatedObjects() {
@@ -202,6 +222,18 @@ public class SellerFormController implements Initializable {
 
 		if (fields.contains("name")) {
 			lblErrorName.setText(errors.get("name"));
+		} else {
+			lblErrorName.setText("");
+		}
+
+		lblErrorEmail.setText((fields.contains("email") ? errors.get("email") : ""));
+
+		lblErrorBaseSalary.setText((fields.contains("baseSalary")) ? errors.get("baseSalary") : "");
+
+		if (fields.contains("birthDate")) {
+			lblErrorBirthDate.setText(errors.get("birthDate"));
+		} else {
+			lblErrorBirthDate.setText("");
 		}
 	}
 
